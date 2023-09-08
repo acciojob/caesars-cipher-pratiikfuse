67fccedd697433e30f224f1d33a674731abe111f
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-08 10:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function rot13(encodedStr) {
   // Only change code below this line
 	for(let  i =0 ;i<encodedStr.length;i++)
 		{
-			decodedArr.add(lookup[encodedStr[i]]);
+			decodedArr.push(lookup[encodedStr[i]]);
 		}
   return decodedArr; //return decodedArr
 }
